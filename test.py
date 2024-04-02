2abefa2ef4360ce4d3f5b9a3fe96fbe55b5c1621
Melody <2220582242@qq.com>
2024-04-02 17:41:05 +0800

    增加gui

--- test.py
+++ test.py
@@ -1,21 +1,16 @@
 import os
-import torch
-
+import sys
 os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
-import pytesseract
 from PIL import Image
 from pdf2image import convert_from_path
 from openpyxl import load_workbook
 from openpyxl.drawing.image import Image as ExcelImage
-from openpyxl.utils import get_column_letter, column_index_from_string
 from openpyxl.styles import Alignment
-import re
+from openpyxl.utils import get_column_letter, column_index_from_string
 import easyocr
-import pandas as pd
 
-pdf_folder_path = './pdf/'
+# pdf_folder_path = './pdf/'
 excel_template_path = 'template.xlsx'
-output_excel_path = 'output.xlsx'
 temp_img_path = "./temp_img/"
 
 """ 
@@ -23,8 +18,6 @@ temp_img_path = "./temp_img/"
     pdf_folder_path: pdf文件夹的路径
     return: images，提取出来的图像列表
 """
-
-
 def pdf2img(pdf_folder_path):
     # 初始化列表用于存储从pdf文件中提取出来的图片
     images = []
@@ -43,8 +36,6 @@ def pdf2img(pdf_folder_path):
     images: 图像列表
     return: person_info_img_cropped_list, 用户信息；test_data_img_cropped_list, 测试数据；data_curve_img_cropped_list, 数据曲线图
 """
-
-
 def crop_images(images):
     # 初始化三个列表，用于存储裁剪后的图像（用户信息、测试数据以及数据曲线图）
     person_info_img_cropped_list = []
@@ -112,13 +103,11 @@ def crop_images(images):
     img_cropped_list: 裁剪后的图像列表
     return: person_info_list，识别的用户信息文本列表；test_data_list，识别的测试数据文本列表；test_data_full_list，识别的完整测试数据列表
 """
-
-
 def ocr(person_info_img_cropped_list, test_data_img_cropped_list):
     # 用于存储两部分信息的列表
     person_info_list = []
     test_data_list = []
-    test_data_full_list = []  # 用于存储完整测试数据
+    test_data_full_list = [] # 用于存储完整测试数据
 
     # 储存前三个数字的坐标信息
     coordinate_list = []
@@ -133,16 +122,16 @@ def ocr(person_info_img_cropped_list, test_data_img_cropped_list):
         test_full_data = reader.readtext(f'./temp_img/test_data_{i + 1}_cropped.png')
         test_data = [item[1] for item in test_full_data]
         # 用于保存测试日期，测试时间以及检查结论
-        test_data_info = []
+        test_data_info = [] 
         test_data_info_list = []
         for i, item in enumerate(test_data):
             if item == '测试日期':
-                test_data_info.append(test_data[i + 1])
+                test_data_info.append(test_data[i+1])
             elif item == '测试时间':
-                test_data_info.append(test_data[i + 1])
-            if '意见' in item and i == len(test_data) - 2:
-                test_data_info.append(test_data[i + 1])
-            elif '意见' in item and i == len(test_data) - 1:
+                test_data_info.append(test_data[i+1])
+            if '意见' in item and i == len(test_data)-2:
+                test_data_info.append(test_data[i+1])
+            elif '意见' in item and i == len(test_data)-1:
                 test_data_info.append(None)
         # test_data_list.append(test_data)
         test_data_info_list.append(test_data_info)
@@ -157,8 +146,6 @@ def ocr(person_info_img_cropped_list, test_data_img_cropped_list):
     data_list: 测试数据列表
     return: base_coordinate_list，基准元素坐标列表
 """
-
-
 def extract_base_coordinate(data_list):
     base_coordinate = []
     base_coordinate_list = []
@@ -182,13 +169,13 @@ def extract_base_coordinate(data_list):
 
 
 """ 判断字符串是否为小数 """
-# def is_float(s):
-#     try:
-#         float(s)
-#         if not s.isdigit():
-#             return True
-#     except ValueError:
-#         return False
+def is_float(s):
+    try:
+        float(s)
+        if not s.isdigit():
+            return True
+    except ValueError:
+        return False
 
 """ 
     预处理ocr识别出来的信息
@@ -197,8 +184,6 @@ def extract_base_coordinate(data_list):
     base_coordinate_list: 基准元素坐标列表
     return: person_info_dict_list, 用户信息字典列表；data_matrix_list, 测试数据矩阵列表
 """
-
-
 def preprocess_text(person_info_list, test_full_data_list, base_coordinate_list):
     # 加载Excel工作簿和工作表
     template_excel_path = 'template.xlsx'
@@ -241,111 +226,95 @@ def preprocess_text(person_info_list, test_full_data_list, base_coordinate_list)
     for base_coordinate in base_coordinate_list:
         # 初始化一个22x3的矩阵，用于暂时存储数据
         data_matrix = [[0 for _ in range(3)] for _ in range(22)]
-
+        
         for test_full_data in test_full_data_list:
             i = 0
             for j, item in enumerate(test_full_data):
                 # print(test_full_data[j])
-                if (base_coordinate[0] - 10 <= item[0][1][0] <= base_coordinate[0] + 20) and \
-                        data_matrix[i][0] == 0:
+                if (base_coordinate[0]-10 <= item[0][1][0] and item[0][1][0] <= base_coordinate[0]+20) and data_matrix[i][0] == 0:
                     data_matrix[i][0] = float(item[1])
-                elif (base_coordinate[1] - 10 <= item[0][1][0] <= base_coordinate[1] + 28) and \
-                        data_matrix[i][1] == 0:
+                elif (base_coordinate[1]-10 <= item[0][1][0] and item[0][1][0] <= base_coordinate[1]+28) and data_matrix[i][1] == 0:
                     data_matrix[i][1] = float(item[1])
                     if data_matrix[i][0] == 0:
                         i += 1
-                elif (base_coordinate[2] - 10 <= item[0][1][0] <= base_coordinate[2] + 34) and \
-                        data_matrix[i][2] == 0:
+                elif (base_coordinate[2]-10 <= item[0][1][0] and item[0][1][0] <= base_coordinate[2]+34) and data_matrix[i][2] == 0:
                     data_matrix[i][2] = float(item[1])
                     i += 1
         for row in data_matrix:
             if row[0] == 0:
-                row[0] = None
+                row[0]=None
             if row[1] == 0:
-                row[1] = None
+                row[1]=None
             if row[2] == 0:
-                row[2] = None
+                row[2]=None 
         data_matrix_list.append(data_matrix)
-
+    
     return person_info_dict_list, data_matrix_list
 
-
 """ 文本插入excel """
-
-
-def text2excel(person_info_dict_list, data_matrix_list, test_data_info_list, data_curve_img_cropped_list):
+def text2excel(person_info_dict_list, data_matrix_list,test_data_info_list,data_curve_img_cropped_list): 
     # 加载Excel工作簿和工作表
     template_excel_path = 'template.xlsx'
     # 加载Excel工作簿和工作表
     workbook = load_workbook(template_excel_path)
     sheet = workbook.active  # 假设数据填充在第一个工作表
 
-    # 居中对其
+    # 居中对齐
     aligin_center = Alignment(horizontal='center', vertical='center')
 
     # 填充患者个人数据到Excel表格
-    for person_info_dict in person_info_dict_list:
-        for i, (key, value) in enumerate(person_info_dict.items(), start=1):
-            cell = sheet.cell(row=2, column=i, value=value)
+    for i, person_info_dict in enumerate(person_info_dict_list,start=2):
+        for j, (key, value) in enumerate(person_info_dict.items(), start=1):
+            cell = sheet.cell(row=i, column=j, value=value)
             cell.alignment = aligin_center
 
     # 填充测试数据到Excel表格
-    for test_data in data_matrix_list:
+    for k, test_data in enumerate(data_matrix_list, start=2):
         # 从第14列开始填充数据
         start_column = 14
         # 填充矩阵到第二行
         count = 0
         for i, row_data in enumerate(test_data):
             for j, cell_data in enumerate(row_data):
-                cell = sheet.cell(row=2, column=start_column + count, value=cell_data)
+                cell = sheet.cell(row=k, column=start_column + count, value=cell_data)
                 cell.alignment = aligin_center
                 count += 1
 
     total_columns = sheet.max_column
     second_last_column = total_columns - 2  # 倒数第二列
     # 填充其他信息到Excel表格
-    for test_data_info in test_data_info_list:
-        sheet.cell(row=2, column=12, value=test_data_info[0]).alignment = aligin_center
-        sheet.cell(row=2, column=13, value=test_data_info[1]).alignment = aligin_center
-        sheet.cell(row=2, column=second_last_column, value=test_data_info[2]).alignment = aligin_center
+    for i, test_data_info in enumerate(test_data_info_list, start=2):
+        sheet.cell(row = i, column = 12, value = test_data_info[0]).alignment = aligin_center
+        sheet.cell(row = i, column = 13, value = test_data_info[1]).alignment = aligin_center
+        sheet.cell(row = i, column = second_last_column, value = test_data_info[2]).alignment = aligin_center
 
     # 填充数据曲线图到excel表格
-    last_column = total_columns - 1  # 最后一列
-    for i, data_curve_img_cropped in enumerate(data_curve_img_cropped_list):
-        excel_image = ExcelImage(f'./temp_img/data_curve_{i+1}_cropped.png')
-        sheet.column_dimensions[get_column_letter(sheet.max_column)].width = 36.3
-        sheet.row_dimensions[i+2].height = 380
-        excel_image.height = 500
-        # sheet.add_image(excel_image,anchor=f"{col}{row}")
-        sheet.add_image(excel_image, f"{get_column_letter(sheet.max_column)}{i+2}")
-        # print(type(data_curve_img_cropped))
+    last_column = total_columns - 1 # 最后一列
+    for i, data_curve_img_cropped in enumerate(data_curve_img_cropped_list, start=1):
+        """ sheet.column_dimensions[get_column_letter(sheet.max_column)].width = data_curve_img_cropped.width
+        sheet.row_dimensions[2].height = data_curve_img_cropped.height """
+        excel_image = ExcelImage(f'./temp_img/data_curve_{i}_cropped.png')
+        sheet.column_dimensions[get_column_letter(sheet.max_column)].width = 40 # 修改最后一列的宽度
+        sheet.row_dimensions[i+1].height = 380
+        excel_image.height = 500 # 修改原图片高度
+        sheet.add_image(excel_image, f"{get_column_letter(sheet.max_column)}2")
 
     # 保存工作簿
     workbook.save('template.xlsx')
 
 
-def main():
+def main(pdf_folder_path):
     images = []
     images = pdf2img(pdf_folder_path)
     person_info_img_cropped_list, test_data_img_cropped_list, data_curve_img_cropped_list = crop_images(images)
-    person_info_list, test_data_info_list, test_full_data_list = ocr(person_info_img_cropped_list,
-                                                                     test_data_img_cropped_list)
-    base_coordinate_list = extract_base_coordinate(test_full_data_list)  # 提取前三个数字坐标的第一个数据，用作后续数据的定位
-    # print(base_coordinate_list)
-    person_info_dict_list, data_matrix_list = preprocess_text(person_info_list, test_full_data_list,
-                                                              base_coordinate_list)
-    # 测试
-    """ for person_info_dict in person_info_dict_list:
-        for key, value in person_info_dict.items():
-            print(f'{key}: {value}')
-    for data_matrix in data_matrix_list:
-        for row in data_matrix:
-            print(row) """
-    text2excel(person_info_dict_list, data_matrix_list, test_data_info_list, data_curve_img_cropped_list)
-    for test_data_info in test_data_info_list:
-        for item in test_data_info:
-            print(item)
-
+    person_info_list, test_data_info_list,test_full_data_list = ocr(person_info_img_cropped_list, test_data_img_cropped_list)
+    base_coordinate_list = extract_base_coordinate(test_full_data_list) # 提取前三个数字坐标的第一个数据，用作后续数据的定位
+    person_info_dict_list, data_matrix_list = preprocess_text(person_info_list, test_full_data_list, base_coordinate_list)
+    text2excel(person_info_dict_list, data_matrix_list,test_data_info_list, data_curve_img_cropped_list)
 
 if __name__ == '__main__':
-    main()
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <pdf_folder_path>")
+        sys.exit(1)
+    pdf_folder_path = sys.argv[1]
+    main(pdf_folder_path)
